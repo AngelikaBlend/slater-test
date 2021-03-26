0bf00817897a38444155f54a5d25289277be174d
Angelika Klimek <angelika@blendcommerce.com>
2021-03-26 10:55:28 +0000

    init

---
+++ slater.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  themes: {
+    development: {
+      id: '120183488589',
+      password: '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',
+      store: 'angelika-klimek.myshopify.com',
+      ignore: [
+        'settings_data.json' // leave this here to avoid overriding theme settings on sync
+      ]
+    }
+  }
+}
